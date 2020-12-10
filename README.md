c4a680db6dd38a9518f8a5e2b965c38fa0650364
Mark Duenas <markgdlduenas@gmail.com>
2020-12-10 13:08:54 -0800

    displayed images

---
+++ README.md
@@ -0,0 +1,9 @@
+### Number and name of feauture: 3 features
+
+### Estimate of time needed to complete: 5hrs
+
+### Start time: 6:55pm
+
+### Finish time:
+
+### Actual time needed to complete:
---
+++ js/app.js
@@ -0,0 +1,41 @@
+$.ajax('../data/page-1.json')
+    .then( data => {
+        data.forEach(animal => {
+            let $imgTemplate = $('img:last-of-type');
+            let $newImage = $imgTemplate.clone();
+            $newImage.attr('src', `${animal.image_url}`);
+            $newImage.attr('alt', `${animal.keyword}`);
+            // $('#photo-template').append(`<h2> ${animal.title} </h2>`);
+            $('#photo-template').append($newImage);
+
+            $newImage.removeAttr('class');
+
+            $('.img').remove();
+
+        })
+    });
+
+
+$.ajax('../data/page-1.json')
+    .then( data => {
+        data.forEach(animal => {
+            let $option = $('option:last-of-type');
+            let $newOption = $option.clone();
+
+            $newOption.attr('value', `${animal.keyword}`);
+            $newOption.text(`${animal.keyword}`);
+
+            $('.filter').append($newOption);
+        })
+    })
+
+
+$('.filter').on('click', function() {
+    let value = this.value;
+
+    let allImagesValue = $('img').attr('alt');
+
+    // console.log(value);
+    console.log(allImagesValue);
+})
+
